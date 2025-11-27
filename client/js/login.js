847baf0e435f44c0d350f70f5eca4743e05bdbb6
Animesh478 <animeshmohanty.am@gmail.com>
2025-11-27 18:52:31 +0530

    feat:login frontend completed

---
+++ client/js/login.js
@@ -0,0 +1,22 @@
+const form = document.querySelector(".login-form");
+
+const loginUser = async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const userObj = { email, password };
+  console.log(userObj);
+  try {
+    await axios.post("http://localhost:8000/user/login", userObj);
+  } catch (error) {
+    console.log(error);
+  }
+
+  form.reset();
+  form.querySelector(":focus")?.blur(); //? removes the focus from the element that is focused upon
+};
+
+form.addEventListener("submit", loginUser);
