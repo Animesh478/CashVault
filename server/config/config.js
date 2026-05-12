a2d3e39498b768bc0506d124ee9820304969afe2
Animesh478 <animeshmohanty.am@gmail.com>
2026-05-12 13:23:17 +0530

    minor changes

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
+console.log("inside config file");
+console.log("dialect-", process.env.DB_DIALECT);
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
