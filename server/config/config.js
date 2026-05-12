88137e4f1d094747faeb66b905cc1bf5ff1e4a88
Animesh478 <animeshmohanty.am@gmail.com>
2026-05-12 13:34:49 +0530

    minor changes

--- server/config/config.js
+++ server/config/config.js
@@ -12,4 +12,11 @@ module.exports = {
     host: process.env.DB_HOST.trim(),
     dialect: "mysql",
   },
+  production: {
+    username: process.env.DB_USERNAME.trim(),
+    password: process.env.DB_PASSWORD.trim(),
+    database: process.env.DB_NAME.trim(),
+    host: process.env.DB_HOST.trim(),
+    dialect: "mysql",
+  },
 };
