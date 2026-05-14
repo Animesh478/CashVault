dab0c7b57800da3e6fe63f644313560aece99e6f
Animesh478 <animeshmohanty.am@gmail.com>
2026-05-14 11:28:06 +0530

    minor changes

--- client/js/index.js
+++ client/js/index.js
@@ -1,11 +1,11 @@
-import { FRONTEND_BASE } from "/config.js";
+import { FRONTEND_BASE } from "./config.js";
 
 const signUpBtn = document.querySelector(".cta-signUp");
 const loginBtn = document.querySelector(".cta-login");
 
 signUpBtn.addEventListener("click", () => {
-  window.location.href = `${FRONTEND_BASE}pages/sign-up.html`;
+  window.location.href = `${FRONTEND_BASE}client/pages/sign-up.html`;
 });
 loginBtn.addEventListener("click", () => {
-  window.location.href = `${FRONTEND_BASE}pages/login.html`;
+  window.location.href = `${FRONTEND_BASE}client/pages/login.html`;
 });
